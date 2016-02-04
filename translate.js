e5ac2d25a5ef1c66a2e646240647448b94cd8253
blake <blakehunsicker@gmail.com>
2016-02-04 16:53:45 -0500

    adding fixes

--- translate.js
+++ translate.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	alert('hello');
 	$('.translate').click(function() {
+		$(this).text('Read in English');
+		$('.headline-kicker').next('h1').text('Zika artigo em Português');
 		$('#article-body article p').toggle();
 		console.log('This better work');
 	});
