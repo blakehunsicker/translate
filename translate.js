2c19bce9799722f1fee52de9b8e2f3c59fa60ff3
blake <blakehunsicker@gmail.com>
2016-02-04 16:58:18 -0500

    no h1 or button toggle

--- translate.js
+++ translate.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	alert('hello');
 	$('.translate').click(function() {
-		$(this).text('Read in English');
-		$('.headline-kicker').next('h1').text('Zika artigo em Português');
+		// $(this).text('Read in English');
+		// $('.headline-kicker').next('h1').text('Zika artigo em Português');
 		$('#article-body article p').toggle();
 		console.log('This better work');
 	});
