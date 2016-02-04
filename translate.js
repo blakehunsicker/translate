879111b55fd24bcf3f8dd7c0c6663f70528e7307
blake <blakehunsicker@gmail.com>
2016-02-04 18:04:42 -0500

    adding flags

--- translate.js
+++ translate.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	alert('hello');
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/lipis/flag-icon-css/master/css/flag-icon.min.css">');
 	$('.translate').click(function() {
 		// $(this).text('Read in English');
 		// $('.headline-kicker').next('h1').text('Zika artigo em Português');
 		$('#article-body article p').toggle();
+		$('.flag-icon').toggle();
 		$('.translate-button').toggle();
-		console.log('This better work');
 	});
 });
