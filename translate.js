41c7b4b1aedcd2be7dbd576f34b49a250c1f92dc
blake <blakehunsicker@gmail.com>
2016-02-04 18:57:30 -0500

    adding crazy headlines

--- translate.js
+++ translate.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/lipis/flag-icon-css/master/css/flag-icon.min.css">');
+	var OG_headline = document.getElementsByClassName('headline-kicker').next('h1').innerHTML;
 	$('.translate').click(function() {
 		// $(this).text('Read in English');
-		// $('.headline-kicker').next('h1').text('Zika artigo em Português');
+		$('.headline-kicker').next('h1').toggleClass('.translated');
+		if ($('.headline-kicker').hasClass('translated')) {
+			$(this).html('Zika história em Português');
+		} else {
+			$(this).html(OG_headline);
+		}
 		$('#article-body article p').toggle();
 		$('.flag-icon').toggle();
 		$('.translate-button').toggle();
