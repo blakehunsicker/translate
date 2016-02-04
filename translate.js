8f64e82b1ff9c8075631cbab5d3a086947fee72b
blake <blakehunsicker@gmail.com>
2016-02-04 16:40:10 -0500

    adding children

--- translate.js
+++ translate.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	alert('hello');
 	$('.translate').click(function() {
-		$('#article-body article').children().toggle();
+		$('#article-body article').children('p').toggle();
 		console.log('This better work');
 	});
 });
