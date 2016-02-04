1ab7434ef920dea119c0e083c53dd679df304e16
blake <blakehunsicker@gmail.com>
2016-02-04 17:03:32 -0500

    push buttons

--- translate.js
+++ translate.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 		// $(this).text('Read in English');
 		// $('.headline-kicker').next('h1').text('Zika artigo em Português');
 		$('#article-body article p').toggle();
+		$('.translate-button').toggle();
 		console.log('This better work');
 	});
 });
