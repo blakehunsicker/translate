061ac6e6918583e739d865f6081214cf71bb667a
blake <blakehunsicker@gmail.com>
2016-02-05 10:21:01 -0500

    moving console

--- translate.js
+++ translate.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/lipis/flag-icon-css/master/css/flag-icon.min.css">');
 	var OG_headline = document.getElementsByClassName('headline-kicker').innerHTML;
-	console.log('original headline is '+OG_headline);
 	$('.translate').click(function() {
+		console.log('original headline is '+OG_headline);
 		// $(this).text('Read in English');
 		$('.headline-kicker').next('h1').toggleClass('.translated');
 		if ($('.headline-kicker').hasClass('translated')) {
